930209bec7c27c61859af51bdbadf8582e0a1cc8
Stephen Badger <stephen.badger@gmail.com>
2020-03-22 16:39:08 +0000

    Parse latest stable and beta versions

--- discover-releases.sh
+++ discover-releases.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
+VERSIONS=($(curl -s http://www.byond.com/download/version.txt))
+echo "Stable version is ${VERSIONS[0]}"
+if [ "${#VERSIONS[@]}" -gt "1" ]; then
+  echo "Beta version is ${VERSIONS[1]}"
+fi
 exit 1
