86aef1e0c3712edc88035a41ae561dd691e93367
Stephen Badger <stephen.badger@gmail.com>
2020-03-22 17:17:04 +0000

    Extract releases into shared folder

--- Jenkinsfile
+++ Jenkinsfile
@@ -18,9 +18,9 @@ pipeline {
         container('bash') {
           script {
             status = sh(script: './discover-releases.sh', returnStatus: true)
-            if (status == 0) {
+            if (status == 1) {
               currentBuild.result = 'SUCCESS'
-            } else if (status == 1) {
+            } else if (status == 0) {
               currentBuild.result = 'NOT_BUILT'
             } else {
               currentBuild.result = 'FAILURE'
--- discover-releases.sh
+++ discover-releases.sh
@@ -1,10 +1,32 @@
 #!/usr/bin/env bash
 
-mkdir -p /releases/stable /releases/beta
+process_release() {
+  local FOLDER=$1
+  local VERSION=$2
+  local MAJOR_VERSION=$(cut -d '.' -f 1 <<< "${VERSION}")
+
+  if [ ! -d "${FOLDER}/${VERSION}" ]; then
+    mkdir -p ${FOLDER}/${VERSION}
+    curl -o ${FOLDER}/${VERSION}/byond.zip http://www.byond.com/download/build/${MAJOR_VERSION}/${VERSION}_byond_linux.zip
+    unzip ${FOLDER}/${VERSION}/byond.zip 'byond/*' -d ${FOLDER}/${VERSION}
+    rm ${FOLDER}/${VERSION}/byond.zip
+    return 1
+  fi
+  return 0
+}
 
 VERSIONS=($(curl -s http://www.byond.com/download/version.txt))
-echo "Stable version is ${VERSIONS[0]}"
+STABLE_VERSION=${VERSIONS[0]}
+BETA_VERSION=${VERSIONS[1]}
+EXIT=0
+echo "Stable version is ${STABLE_VERSION}"
+process_release /releases/stable ${STABLE_VERSION}
+EXIT != $?
+
 if [ "${#VERSIONS[@]}" -gt "1" ]; then
-  echo "Beta version is ${VERSIONS[1]}"
+  echo "Beta version is ${BETA_VERSION}"
+  process_release /releases/beta ${STABLE_VERSION}
+  EXIT != $?
 fi
-exit 1
+
+exit ${EXIT}
