be376ce8a016a54bee8633cbb58328548f92e0c7
Stephen Badger <stephen.badger@gmail.com>
2020-03-22 16:57:31 +0000

    Create release folders

--- discover-releases.sh
+++ discover-releases.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+mkdir -p /releases/stable /releases/beta
+
 VERSIONS=($(curl -s http://www.byond.com/download/version.txt))
 echo "Stable version is ${VERSIONS[0]}"
 if [ "${#VERSIONS[@]}" -gt "1" ]; then
